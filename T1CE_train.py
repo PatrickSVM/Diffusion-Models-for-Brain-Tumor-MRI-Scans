59e9a0dd9ccbce84bc9def2ce65ccc20b08e4709
Patrick Siegfried Hiemsch <pathi619@lnx00276.ad.liu.se>
2022-11-16 09:54:11 +0100

    Fixed Training variable problem, Started trainig

---
+++ T1CE_train.py
@@ -0,0 +1,10 @@
+import os
+
+if __name__ == "__main__":
+    MODEL_FLAGS="--image_size 256 --num_channels 128 --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
+    DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False"
+    TRAIN_FLAGS="--lr 1e-4 --batch_size 1"
+
+    CALL=f"python image_train.py --log_interval 10 --save_interval 5000 --data_dir brats2020_313subjects_noaugmentation_singlesegmentationchannel_min15percentcoverage/T1CE --out_dir Checkpoint_T1CE {MODEL_FLAGS} {DIFFUSION_FLAGS} {TRAIN_FLAGS}"
+    
+    os.system(CALL)
--- cifar_training.py
+++ cifar_training.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine --use_kl True"
     TRAIN_FLAGS="--lr 1e-4 --batch_size 128 --schedule_sampler loss-second-moment"
 
-    os.system('python image_train.py --log_interval 1000 --save_interval 25000 --data_dir cifar_train --out_dir Checkpoint_CIFAR $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS')
+    CALL=f'python image_train.py --log_interval 1000 --save_interval 25000 --data_dir cifar_train --out_dir Checkpoint_CIFAR {MODEL_FLAGS} {DIFFUSION_FLAGS} {TRAIN_FLAGS}'
 
-    
+    os.system(CALL)
