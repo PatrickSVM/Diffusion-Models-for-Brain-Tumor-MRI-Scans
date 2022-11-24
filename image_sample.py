148f57648ee80052787a3a055c1294a982e9162a
Patrick Siegfried Hiemsch <pathi619@lnx00276.ad.liu.se>
2022-11-24 14:13:25 +0100

    First samples from one channel model

--- image_sample.py
+++ image_sample.py
@@ -51,7 +51,7 @@ def main():
         )
         sample = sample_fn(
             model,
-            (args.batch_size, 3, args.image_size, args.image_size),
+            (args.batch_size, 1, args.image_size, args.image_size), # Added, TODO
             clip_denoised=args.clip_denoised,
             model_kwargs=model_kwargs,
         )
